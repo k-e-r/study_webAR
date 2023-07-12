686103e90f01f2f45b8b6f5fa7c82e1883c97f1e
k-e-r <damajonboua@gmail.com>
2023-07-11 17:16:36 -0700

    modify mind image

--- works/MealOnTheTable/main.js
+++ works/MealOnTheTable/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targets/0fool.mind',
+      imageTargetSrc: '../../assets/targets/targets.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
