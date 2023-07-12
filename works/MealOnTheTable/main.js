efd005cb9db9e7991f84e2e2d3e56258fb2ed8dd
k-e-r <damajonboua@gmail.com>
2023-07-11 18:29:36 -0700

    modify ar marker

--- works/MealOnTheTable/main.js
+++ works/MealOnTheTable/main.js
@@ -1,62 +1,36 @@
-import * as THREE from '../../libs/three.js-r132/build/three.module.js';
 import { loadGLTF } from "../../libs/loader.js";
-// const THREE = window.MINDAR.IMAGE.THREE;
+const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const scene = new THREE.Scene();
-
-  const camera = new THREE.PerspectiveCamera();
-  camera.position.set(1, 1, 5);
-
-  const renderer = new THREE.WebGLRenderer({alpha: true});
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.render(scene, camera);
-
-  const video = document.createElement("video");
-  navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
-    video.srcObject = stream;
-    video.play();
-  });
-
-  video.style.position = "absolute";
-  video.style.width = renderer.domElement.width;
-  video.style.height = renderer.domElement.height;
-  renderer.domElement.style.position = "absolute";
-
-  document.body.appendChild(video);
-  document.body.appendChild(renderer.domElement);
-
   const start = async() => {
-    // const mindarThree = new window.MINDAR.IMAGE.MindARThree({
-    //   container: document.body,
-    //   imageTargetSrc: '../../assets/targets/0fool.mind',
-    // });
-    // const {renderer, scene, camera} = mindarThree;
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      imageTargetSrc: '../../assets/targets/pattern-marker.mind',
+    });
+    const {renderer, scene, camera} = mindarThree;
 
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
     // const gltf = await loadGLTF('../../assets/models/musicband-raccoon/scene.gltf');
     const burger = await loadGLTF('./Burger-Only.glb');
-    burger.scene.scale.set(0.5, 0.5, 0.5);
+    burger.scene.scale.set(1, 1, 1);
     burger.scene.rotation.set(1.25, 0, 0);
-    burger.scene.position.set(0, 0, 0);
+    burger.scene.position.set(-2, -2, 0);
     const ebiSushi = await loadGLTF('./Ebi-Sushi.glb');
-    ebiSushi.scene.scale.set(0.8, 0.8, 0.8);
+    ebiSushi.scene.scale.set(1, 1, 1);
     ebiSushi.scene.rotation.set(1.25, 0, 0);
-    ebiSushi.scene.position.set(0, 2, 0);
+    ebiSushi.scene.position.set(-2, 2, 0);
     const ikuraSushi = await loadGLTF('./Ikura-Sushi.glb');
-    ikuraSushi.scene.scale.set(0.8, 0.8, 0.8);
+    ikuraSushi.scene.scale.set(1, 1, 1);
     ikuraSushi.scene.rotation.set(1.25, 0, 0);
-    ikuraSushi.scene.position.set(0.5, 2.2, 0);
+    ikuraSushi.scene.position.set(-1, 2, 0);
 
-    // const anchor = mindarThree.addAnchor(0);
-    // anchor.group.add(burger.scene);
-    // anchor.group.add(ebiSushi.scene);
-    // anchor.group.add(ikuraSushi.scene);
-    scene.add(burger.scene);
-    scene.add(ebiSushi.scene);
-    scene.add(ikuraSushi.scene);
+    const anchor = mindarThree.addAnchor(0);
+    anchor.group.add(burger.scene);
+    anchor.group.add(ebiSushi.scene);
+    anchor.group.add(ikuraSushi.scene);
+    scene.add(burger.scene)
 
     const mixer = new THREE.AnimationMixer(burger.scene);
     // const action = mixer.clipAction(gltf.animations[0]);
@@ -64,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const clock = new THREE.Clock();
 
-    // await mindarThree.start();
+    await mindarThree.start();
     renderer.setAnimationLoop(() => {
       const delta = clock.getDelta();
       // gltf.scene.rotation.set(gltf.scene.rotation.x+delta, gltf.scene.rotation.y, 0);
